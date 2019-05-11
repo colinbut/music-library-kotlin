26bc0e8751f53516774e32d7a631609ca74a1d6e
Colin But <colinbut@users.noreply.github.com>
2019-05-11 10:32:21 +0100

    Application class

---
+++ src/main/kotlin/com/mycompany/musicfy/Application.kt
@@ -0,0 +1,10 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2019 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.musicfy
+
+fun main(args: Array<String>) {
+    println("Musicfy")
+}
