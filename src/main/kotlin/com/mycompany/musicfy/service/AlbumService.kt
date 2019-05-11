e926e015edde29de70b1361ef88ed4220cd8d628
Colin But <colinbut@users.noreply.github.com>
2019-05-11 10:27:03 +0100

    services template

---
+++ src/main/kotlin/com/mycompany/musicfy/service/AlbumService.kt
@@ -0,0 +1,10 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2019 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.musicfy.service
+
+interface AlbumService {
+    fun getAlbumList()
+}
---
+++ src/main/kotlin/com/mycompany/musicfy/service/AlbumServiceImpl.kt
@@ -0,0 +1,13 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2019 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.musicfy.service
+
+class AlbumServiceImpl : AlbumService {
+
+    override fun getAlbumList() {
+
+    }
+}
